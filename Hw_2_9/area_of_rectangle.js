b4e73e1c82bbda1019af3da0aae7d405de000a6c
Kostyanoy <kostya211280@gmail.com>
2016-02-11 23:36:40 +0200

    area rectangle

---
+++ Hw_2_9/area_of_rectangle.js
@@ -0,0 +1,22 @@
+function areaRectangle1(a, b){ 
+	var s = a*b;
+	return (s);
+} 
+
+function areaRectangle2(c, d){ 
+	var z = c*d;
+	return (z);
+}
+
+function Max(a, b) {
+ var z=Math.max(a, b);
+	return (z);
+}
+
+
+a = areaRectangle1(2, 5);
+b = areaRectangle2(4, 6);
+
+console.log (a);
+console.log (b);
+console.log (Max(a, b));
